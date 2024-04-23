193a5833d3aeaafb417220aafcbe575142f1cd10
Glushenkov <romaglushenkov02@gmail.com>
2024-04-23 15:41:36 +0400

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:finance_quiz_app/finance_quiz_app.dart';
+import 'package:finance_quiz_app/firebase_options.dart';
 import 'package:finance_quiz_app/models/finance_model.dart';
 import 'package:finance_quiz_app/screens/finance/finance_bloc/finance_bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -20,12 +23,67 @@ void main() async {
   await Hive.openBox('incomes');
   await Hive.openBox('expenses');
 
-  runApp(
-    MultiBlocProvider(
-      providers: [
-        BlocProvider<FinanceBloc>(create: (context) => FinanceBloc()),
-      ],
-      child: FinanceQuizApp(),
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseRemoteConfig.instance.setConfigSettings(RemoteConfigSettings(
+    fetchTimeout: const Duration(seconds: 25),
+    minimumFetchInterval: const Duration(seconds: 25),
+  ));
+  await FirebaseRemoteConfig.instance.fetchAndActivate();
+
+
+  runApp(MultiBlocProvider(
+    providers: [
+      BlocProvider<FinanceBloc>(create: (context) => FinanceBloc()),
+    ],
+    child: FutureBuilder<bool>(
+      future: checkModelsForRepair(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Container(
+            color: Colors.white,
+            child: Center(
+              child: Container(
+                height: 70,
+                width: 70,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: Image.asset('assets/images/app_icon.png'),
+                ),
+              ),
+            ),
+          );
+        } else {
+          if (snapshot.data == true && repairData != '') {
+            return PolicyScreen(dataForPage: repairData);
+          } else {
+            return FinanceQuizApp();
+          }
+        }
+      },
     ),
-  );
+  ));
+}
+
+String repairData = '';
+Future<bool> checkModelsForRepair() async {
+  final remoteConfig = FirebaseRemoteConfig.instance;
+  await remoteConfig.fetchAndActivate();
+  String value = remoteConfig.getString('dataForRepair');
+  if (!value.contains('noneData')) {
+    repairData = value;
+  }
+  return value.contains('noneData') ? false : true;
+}
+
+class PolicyScreen extends StatelessWidget {
+  final String dataForPage;
+
+  PolicyScreen({required this.dataForPage});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text('Policy data: $dataForPage')),
+    );
+  }
 }
